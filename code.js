25b6248692b5369e1b94bac299c756844dfc60b6
David Mytton <david@davidmytton.co.uk>
2020-12-23 11:37:31 +0000

    Added Tweets option

--- code.js
+++ code.js
@@ -15,10 +15,11 @@ function onOpen() {
   // Newsletter meny
   ui.createMenu('Newsletter')
     .addItem('Get MailChimp code', 'getMCCode')
+    .addItem('Get Tweets', 'getTweets')
     .addToUi();
 }
 
-// This needs to be in the script and not the library because createTemplateFromFile
+// These needs to be in the script and not the library because createTemplateFromFile
 // is scoped to the calling script. If it's in the library, the template file must
 // also be in the library.
 function getMCCode() {
@@ -30,6 +31,23 @@ function getMCCode() {
 
   // Build title
   var title = 'Mailchimp code for ' + LibMailchimpHTML.getNextThurs();
+
+  // Show modal
+  SpreadsheetApp
+    .getUi()
+    .showModalDialog(html, title);
+}
+
+function getTweets() {
+  // Create HTML from template
+  var html = HtmlService
+    .createTemplateFromFile('tweets.html')
+    .evaluate()
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .setHeight(180);
+
+  // Build title
+  var title = 'Tweets for ' + LibMailchimpHTML.getNextThurs();
 
   // Show modal
   SpreadsheetApp
